ce571b33ce547f9d5e1e0f29c38e9af20fd597f3
Bappy Gazi <41175073+bappygazi@users.noreply.github.com>
2023-06-23 03:51:35 +0600

    achages

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-//testimonial slider
-$(".slider").slick({
-    prevArrow:'<i class="fa-solid fa-angle-left text-white prev"></i>',
-    nextArrow:'<i class="fa-solid fa-angle-right text-white next"></i>'
-});
+$(function () {
+  // animation scroll js
+  var html_body = $('html, body');
+  $('.navbar a').on('click', function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        html_body.animate({
+          scrollTop: target.offset().top - 90
+        }, 1400);
+        return false;
+      }
+    }
+  });
 
-// books slider
+  //testimonial slider
+  $(".slider").slick({
+    prevArrow: '<i class="fa-solid fa-angle-left text-white prev"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-right text-white next"></i>'
+  });
 
-$('.books-slider').slick({
+  // books slider
+
+  $('.books-slider').slick({
     dots: true,
     speed: 300,
     slidesToShow: 5,
     centerPadding: '60px',
     slidesToScroll: 1,
-    prevArrow:'<i class="fa-solid fa-angle-left text-white prev"></i>',
-    nextArrow:'<i class="fa-solid fa-angle-right text-white next"></i>',
-    responsive: [
-      {
+    prevArrow: '<i class="fa-solid fa-angle-left text-white prev"></i>',
+    nextArrow: '<i class="fa-solid fa-angle-right text-white next"></i>',
+    responsive: [{
         breakpoint: 992,
         settings: {
           centerPadding: '40px',
@@ -35,3 +50,5 @@ $('.books-slider').slick({
       }
     ]
   });
+
+});
